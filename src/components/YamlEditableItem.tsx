0fe11808676f8540f0039438e56595fe88bb0b34
Steve <steve.hopkins@outlook.com>
2024-10-27 11:37:16 +0000

    Added an optional unit argument to integer and enum item types (displayed after field name).

--- src/components/YamlEditableItem.tsx
+++ src/components/YamlEditableItem.tsx
@@ -1,7 +1,7 @@
 // This file defines the YamlEditableItem component which handles the rendering and editing of individual YAML items,
 // supporting different metadata types such as string, date, integer, and enum.
 
-import { isPair, isScalar} from "yaml";
+import { isPair, isScalar } from "yaml";
 import { Form } from "react-bootstrap";
 import { MetadataType, parseItem } from "../yaml/metadata";
 
@@ -55,13 +55,14 @@ export function YamlEditableItem(props: YamlEditableItemProps) {
                 const minInt = meta!.args.has('min') ? meta!.args.get('min') : undefined;
                 const maxInt = meta!.args.has('max') ? meta!.args.get('max') : undefined;
                 const display = meta!.args.has('display') ? meta!.args.get('display') : 'box';
+                const unitInt = meta!.args.has('unit') ? meta!.args.get('unit') : undefined;
 
                 switch (display) {
                     case 'box':
                         // Display integer input as a number box
                         return (
                             <>
-                                <Form.Label><strong>{meta?.fieldName}</strong></Form.Label>
+                                <Form.Label><strong>{meta?.fieldName}</strong> {unitInt ? '(' + unitInt + ')' : ''}</Form.Label>
                                 <Form.Control type='number' value={props.item.value.value as string} min={minInt} max={maxInt} onChange={(e: any) => props.onChange(parseInt(e.target.value))} />
                                 <Form.Text>{minInt ? 'min: ' + minInt : ''} &nbsp; {maxInt ? 'max: ' + maxInt : ''}</Form.Text>
                             </>
@@ -71,7 +72,7 @@ export function YamlEditableItem(props: YamlEditableItemProps) {
                         // Display integer input as a slider
                         return (
                             <>
-                                <Form.Label><strong>{meta?.fieldName}</strong></Form.Label>
+                                <Form.Label><strong>{meta?.fieldName}</strong> {unitInt ? '(' + unitInt + ')' : ''}</Form.Label>
                                 <Form.Control type='range' value={props.item.value.value as string} min={minInt} max={maxInt} onChange={(e: any) => props.onChange(parseInt(e.target.value))} />
                                 <Form.Text>{minInt ? 'min: ' + minInt : ''} &nbsp; {maxInt ? 'max: ' + maxInt : ''}</Form.Text>
                             </>
@@ -85,9 +86,10 @@ export function YamlEditableItem(props: YamlEditableItemProps) {
             case MetadataType.Enum:
                 // Handle enum type metadata with a dropdown selection
                 const elems = meta!.args.has('elems') ? meta!.args.get('elems')!.split(',') : [];
+                const unitEnum = meta!.args.has('unit') ? meta!.args.get('unit') : undefined;
                 return (
                     <>
-                        <Form.Label><strong>{meta?.fieldName}</strong></Form.Label>
+                        <Form.Label><strong>{meta?.fieldName}</strong> {unitEnum ? '(' + unitEnum + ')' : ''}</Form.Label>
                         <Form.Select onChange={(e: any) => props.onChange(e.target.value)}>
                             {elems.map(e => <option value={e} selected={props.item.value.value === e}>{e}</option>)}
                         </Form.Select>
